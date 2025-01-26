33a5a8de42eb6f29e14e3dab5f63f20eb59bc45e
adomfosu2000 <adomfosu2000@gmail.com>
2025-01-26 19:20:38 +0000

    heli

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-
+'use dynamic'
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../(root)/globals.css";
@@ -23,6 +23,7 @@ export const metadata: Metadata = {
   description: "Traverse -Revolutinizing building in Africa - Real Estate",
 };
 
+export const dynamic = "force-dynamic";
 export default async function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) 
 {
  

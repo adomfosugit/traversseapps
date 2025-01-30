7dec6dddef4c87118ada788851a297db0a584cfc
adomfosugit <adomfosugit@gmail.com>
2025-01-30 23:38:42 +0000

    apicki

--- app/api/oauth/route.ts
+++ app/api/oauth/route.ts
@@ -17,5 +17,5 @@ export async function GET(request:any) {
     secure: true,
   });
 
-  return NextResponse.redirect(`${request.nextUrl.origin}/account-recovery`);
+  return NextResponse.redirect(`${request.nextUrl.origin}/dashboard`);
 }
